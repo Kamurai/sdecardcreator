8cc64548665d1cb330d6a60107fec015cfef9186
Kamurai <16156165+Kamurai@users.noreply.github.com>
2025-01-13 15:55:04 +0000

    Keyword corrections

--- script/keyword/KeywordStore.js
+++ script/keyword/KeywordStore.js
@@ -110,12 +110,25 @@ function KeywordStore(keywords){
    * Find keywords that can contain other keywords.
    */
   this.findKeywords=function(text){
-    text = text.replace(this.re,function(match){
-      var key = this.lookup[toCamelCaseLoop(match)];
-      var keyClass = this.resolveKeyClass(key);
-      return '<span class="keyword '+keyClass.toUpperCase()+'" data-key="'+toCamelCaseLoop(key)+'">'+toCamelCaseLoop(key)+'</span>';
+	var resultA = text;
+	var newText = 
+	//text = 
+	text.replace(this.re,function(match){
+		var resultB = '';
+		if(isNotEmpty(match)){
+			var key = this.lookup[toCamelCaseLoop(match)];
+			var keyClass = this.resolveKeyClass(key);
+			resultB = '<span class="keyword '+keyClass.toUpperCase()+'" data-key="'+toCamelCaseLoop(key)+'">'+toCamelCaseLoop(key)+'</span>';
+		}
+
+		return resultB;
     }.bind(this));
-    return text;
+	
+	if(isNotEmpty(newText)) {
+		resultA = newText;
+	}
+	
+    return resultA;
   };
   
   this.findNKeywords=function(text){
